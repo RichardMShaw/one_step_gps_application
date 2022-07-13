fdc177ecb02b67a1c27590aa379d09a3dc177913
Richard Morgan Shaw <richardmorganshaw@gmail.com>
2022-07-12 19:20:37 -0700

    Push

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
@@ -10,16 +9,15 @@ import (
 	"github.com/RichardMShaw/one_step_gps_application/packages/app_config"
 	"github.com/RichardMShaw/one_step_gps_application/packages/db"
 	"github.com/RichardMShaw/one_step_gps_application/packages/routes"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	var app app_config.AppConfig
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// // if err != nil {
+	// // 	("Error loading .env file")
+	// // }
 
 	app.MongoClient = db.InitalizeConnection()
 
